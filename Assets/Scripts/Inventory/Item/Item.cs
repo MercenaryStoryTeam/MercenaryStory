950bb7d7f8bc494ec535428b1379a7cebf7fffbf
Soyoung <soyoung7896@naver.com>
2025-01-06 19:01:30 +0900

    [추가] Item, Slot Script
    추가만 함

---
+++ Assets/Scripts/Inventory/Item/Item.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class Item : MonoBehaviour
+{
+    private int itemPrice = 200;
+    private Image itemImage;
+    private string itemName;
+}
