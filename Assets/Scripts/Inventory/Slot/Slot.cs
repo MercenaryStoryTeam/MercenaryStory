3c189c41966a6f57a611add281496acd97f94e04
Soyoung <soyoung7896@naver.com>
2025-01-06 20:16:09 +0900

    [변경] Slot

--- Assets/Scripts/Inventory/Slot/Slot.cs
+++ Assets/Scripts/Inventory/Slot/Slot.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class Slot : MonoBehaviour
 {
+    public List<Slot> slots;
+
+    private void AddItem()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            slots.Add(new Slot());
+        }
+    }
     
 }
