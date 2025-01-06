97a367f9da6dc2edb9eec9d40115c0028859c5ff
Soyoung <soyoung7896@naver.com>
2025-01-06 20:16:21 +0900

    [변경] Inventory

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,8 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     public List<Item> items;
+
+    private void Update()
+    {
+    }
 }
--- Assets/Scripts/Inventory/TestSY.cs
+++ Assets/Scripts/Inventory/TestSY.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TestSY : MonoBehaviour
 {
@@ -18,8 +19,13 @@ public class TestSY : MonoBehaviour
         invenUI.TryOpenInventory();
     }
 
-    private void Option()
+    private void ItemDropTest()
     {
-        
+        //ItemManager.Instance.AddItem();
+        int randomDrop = Random.Range(0, 4);
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            
+        }
     }
 }
