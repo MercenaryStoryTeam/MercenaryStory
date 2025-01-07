2b8059bcf45e05e1dddb49681f5305d09164a755
Soyoung <soyoung7896@naver.com>
2025-01-07 11:18:35 +0900

    [추가] Item Info Panel

---
+++ Assets/1_Scripts/Inventory/UI/ItemInfoPanel.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemInfoPanel : MonoBehaviour
+{
+    public GameObject itemInfoPanel;
+
+    public Image itemImage;
+    public Text itemName;
+    public Text itemInfo;
+    
+    public Button equipItemButton;
+    public Button removeItemButton;
+    public Button closeButton;
+
+    private void Awake()
+    {
+        itemImage = GetComponent<Image>();
+        itemInfoPanel.SetActive(false);
+        InfoButtonOnClick();
+    }
+
+    private void InfoButtonOnClick()
+    {
+        equipItemButton.onClick.AddListener(EquipItemButtonClick);
+        removeItemButton.onClick.AddListener(RemoveItemButtonClick);
+        closeButton.onClick.AddListener(CloseButtonClick);
+    }
+    
+    private void CloseButtonClick()
+    {
+        itemInfoPanel.SetActive(false);
+    }
+
+    private void RemoveItemButtonClick()
+    {
+        //inven.items.Remove();
+    }
+    
+    private void EquipItemButtonClick()
+    {
+    }
+
+}
