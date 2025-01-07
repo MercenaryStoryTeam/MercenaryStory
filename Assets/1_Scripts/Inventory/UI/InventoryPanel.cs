85f05350469efa3ae1e91fd973b476ee789ad5a5
Soyoung <soyoung7896@naver.com>
2025-01-07 11:18:24 +0900

    [변경] Inventory Panel
    item info panel 이랑 스크립트 나눔

---
+++ Assets/1_Scripts/Inventory/UI/InventoryPanel.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class InventoryPanel : MonoBehaviour
+{
+    
+    public GameObject panel;
+    
+    public Button invenCloseButton;
+    
+    private void Awake()
+    {
+        panel.SetActive(false);
+        ButtonOnClick();
+    }
+
+    private void ButtonOnClick()
+    {
+        invenCloseButton.onClick.AddListener(InvenCloseButtonClick);
+    }
+    
+    private void InvenCloseButtonClick()
+    {
+        panel.SetActive(false);
+        UIManager.Instance.isInventoryActive = false;
+    }
+
+    public void TryOpenInventory()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (!UIManager.Instance.isInventoryActive)
+            {
+                UIManager.Instance.CloseInventoryPanel();
+            }
+            else
+            {
+                UIManager.Instance.OpenInventoryPanel();
+            }
+        }
+    }
+}
