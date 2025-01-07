cf74ca332467c13f48146330b1e3e131170d52b7
Soyoung <soyoung7896@naver.com>
2025-01-07 11:19:11 +0900

    [변경] Item Manager, Test SY
    SingletonManager 사용

--- Assets/1_Scripts/Inventory/TestSY.cs
+++ Assets/1_Scripts/Inventory/TestSY.cs
@@ -6,17 +6,17 @@ using Random = UnityEngine.Random;
 
 public class TestSY : MonoBehaviour
 {
-    private InvenUI invenUI; // 테스트용. 나중에 UIManager로 변경 예정
+    private InventoryPanel _inventoryPanel; // 테스트용. 나중에 UIManager로 변경 예정
     private int currentOption;
     
     private void Awake()
     {
-        invenUI = FindObjectOfType<InvenUI>();
+        _inventoryPanel = FindObjectOfType<InventoryPanel>();
     }
 
     private void Update()
     {
-        invenUI.TryOpenInventory();
+        _inventoryPanel.TryOpenInventory();
     }
 
     private void ItemDropTest()
