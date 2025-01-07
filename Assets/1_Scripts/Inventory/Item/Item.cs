2258e431f2122f72e983d2e96b1b46b14e700d3b
Soyoung <soyoung7896@naver.com>
2025-01-07 19:38:14 +0900

    [변경] Item Script

--- Assets/1_Scripts/Inventory/Item/Item.cs
+++ Assets/1_Scripts/Inventory/Item/Item.cs
@@ -7,5 +7,13 @@ using UnityEngine.UI;
 [Serializable]
 public class Item
 {
-
+    public int item_Id;
+    public string item_Name;
+    public int item_Class;
+    public bool item_Unique;
+    public bool item_Stackposible;
+    public int item_Stackingamount;
+    public int item_Rank;
+    public int item_MaxRank;
+    public float item_Cellprice;
 }
