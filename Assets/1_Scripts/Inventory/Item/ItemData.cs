3f43387e7201a6abb16953759bf2aee4dd5a02e9
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-08 20:31:06 +0900

    [추가] ItemData

---
+++ Assets/1_Scripts/Inventory/Item/ItemData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData
+{
+	public int item_Id { get; private set; }
+	public int item_Count { get; private set; }
+}
