ed9ba3ac4f36addfa7fc336defd224e846b95e8c
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-01-10 05:46:03 +0900

    [변경] AttackState 구현

--- Assets/1_Scripts/Monster/MonsterStates/MonsterAttackState.cs
+++ Assets/1_Scripts/Monster/MonsterStates/MonsterAttackState.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class MonsterAttackState : MonsterState
 {
-    public override void EnterState(Monster entity)
+    public override void EnterState(Monster monster)
     {
-        throw new System.NotImplementedException();
+        monster.Animator.SetTrigger("Attack");
+        
     }
 
-    public override void ExecuteState(Monster entity)
+    public override void ExecuteState(Monster monster)
     {
-        throw new System.NotImplementedException();
+        if (monster.PlayerTransform != null)
+        {
+            Vector3 direction = (monster.PlayerTransform.position - monster.transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            monster.transform.rotation = Quaternion.Slerp(monster.transform.rotation, lookRotation, monster.RotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            monster.ChangeState(MonsterStateType.Chase);
+        }
     }
 
-    public override void ExitState(Monster entity)
+    public override void ExitState(Monster monster)
     {
-        throw new System.NotImplementedException();
+        
     }
 }
