7d6103b35ff00640ae4c39d284a9ae387df5a30c
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-11 14:53:38 +0900

    [변경] PlayerManager -> ServerManager
    연결, 메인 서버 룸(서버1,2) 생성하는 역할

---
+++ Assets/01_Scripts/User/ServerManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.InputSystem.Interactions;
+
+public class ServerManager
+{
+	public static void ConnectLobby()
+	{
+		PhotonNetwork.NickName = FirebaseManager.Instance.CurrentUserData.user_Name;
+		PhotonNetwork.ConnectUsingSettings();
+	}
+
+	public static void JoinOrCreatePersistentRoom(string roomName)
+	{
+		RoomOptions roomOptions = new RoomOptions
+		{
+			IsVisible = true,
+			IsOpen = true,
+			MaxPlayers = 10,
+			EmptyRoomTtl = 60000
+		};
+
+		PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+	}
+}
