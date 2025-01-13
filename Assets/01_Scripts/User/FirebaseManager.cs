61a253a0285648a73881978bb7bf29015a96fc2f
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-13 13:51:25 +0900

    [변경] FirebaseManager
    로그인 예외처리 추가. 이메일이 존재하지 않을 때의 처리는 미구현

--- Assets/01_Scripts/User/FirebaseManager.cs
+++ Assets/01_Scripts/User/FirebaseManager.cs
@@ -70,7 +70,9 @@ public class FirebaseManager : SingletonManager<FirebaseManager>
 	public async void CheckEmail(string email)
 	{
 		DataSnapshot usersData = await DB.GetReference("users").GetValueAsync();
-		userDictionary = JsonConvert.DeserializeObject<Dictionary<string, UserData>>(usersData.GetRawJsonValue());
+		userDictionary =
+			JsonConvert.DeserializeObject<Dictionary<string, UserData>>(
+				usersData.GetRawJsonValue());
 		if (userDictionary != null)
 		{
 			userList = new List<UserData>(userDictionary.Values);
@@ -93,32 +95,59 @@ public class FirebaseManager : SingletonManager<FirebaseManager>
 
 	public async void SignIn(string email, string password)
 	{
-		PanelManager.Instance.popUp.WaitPopUpOpen("로그인 중입니다.");
-		var result = await Auth.SignInWithEmailAndPasswordAsync(email, password);
-		usersRef = DB.GetReference($"users/{result.User.UserId}");
-		DataSnapshot userDataValues = await usersRef.GetValueAsync();
-		UserData userData = null;
-		if (userDataValues.Exists)
+		try
 		{
-			string json = userDataValues.GetRawJsonValue();
-			userData = JsonConvert.DeserializeObject<UserData>(json);
-		}
+			PanelManager.Instance.popUp.WaitPopUpOpen("로그인 중입니다.");
+			var result = await Auth.SignInWithEmailAndPasswordAsync(email, password);
+			usersRef = DB.GetReference($"users/{result.User.UserId}");
+			DataSnapshot userDataValues = await usersRef.GetValueAsync();
+			UserData userData = null;
+			if (userDataValues.Exists)
+			{
+				string json = userDataValues.GetRawJsonValue();
+				userData = JsonConvert.DeserializeObject<UserData>(json);
+			}
 
-		CurrentUserData = userData;
+			CurrentUserData = userData;
 
-		PanelManager.Instance.popUp.PopUpClose();
-		ServerManager.ConnectLobby();
-		if (CurrentUserData.user_Appearance == 0)
-		{
-			PanelManager.Instance.PanelOpen("CharacterSelect");
+			PanelManager.Instance.popUp.PopUpClose();
+			ServerManager.ConnectLobby();
+			if (CurrentUserData.user_Appearance == 0)
+			{
+				PanelManager.Instance.PanelOpen("CharacterSelect");
+			}
+			else
+			{
+				PanelManager.Instance.PanelOpen("ServerSelect");
+			}
 		}
-		else
+		catch (FirebaseException e)
 		{
-			PanelManager.Instance.PanelOpen("ServerSelect");
+			switch (e.ErrorCode)
+			{
+				case 37:
+				case 38:
+					PanelManager.Instance.popUp.PopUpOpen("이메일과 비밀번호를 입력해 주세요",
+						() => PanelManager.Instance.popUp.PopUpClose());
+					break;
+				case 1:
+					PanelManager.Instance.popUp.PopUpOpen("이메일과 비밀번호가 일치하지 않거나\n존재하지 않는 이메일입니다.",
+						() => PanelManager.Instance.popUp.PopUpClose());
+					break;
+				case 11:
+					PanelManager.Instance.popUp.PopUpOpen("이메일의 형식이 아닙니다.",
+						() => PanelManager.Instance.popUp.PopUpClose());
+					break;
+				default:
+					PanelManager.Instance.popUp.PopUpOpen(e.Message,
+						() => PanelManager.Instance.popUp.PopUpClose());
+					break;
+			}
 		}
 	}
 
-	public async void UpdateCurrentUserData(string childName, object value, Action<object> callback = null)
+	public async void UpdateCurrentUserData(string childName, object value,
+		Action<object> callback = null)
 	{
 		DatabaseReference targetRef = usersRef.Child(childName);
 		await targetRef.SetValueAsync(value);
