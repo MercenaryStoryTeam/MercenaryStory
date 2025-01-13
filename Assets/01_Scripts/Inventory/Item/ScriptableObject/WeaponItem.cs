dc0c47e3b7ba524413e773ef80858d0551071c51
Soyoung <soyoung7896@naver.com>
2025-01-13 18:08:26 +0900

    [변경] Items

--- Assets/01_Scripts/Inventory/Item/ScriptableObject/WeaponItem.cs
+++ Assets/01_Scripts/Inventory/Item/ScriptableObject/WeaponItem.cs
@@ -10,12 +10,9 @@ public class WeaponItem : ItemBase
 {
     public int Rank; // 아이템 등급
     public float Damage; // 공격력
-    public float sellPrice; // 물품 가격
     public bool isEquiped; // 장착 여부
     private void Awake()
     {
-        isSellable = true;
-        isStackable = false;
         isEquiped = false;
     }
     
