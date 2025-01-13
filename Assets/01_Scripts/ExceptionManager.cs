d22c98e1189bc469553bb7bb9a09a8d64f6cbcc1
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-13 14:16:48 +0900

    [추가] ExceptionManager
    예외처리를 위함

---
+++ Assets/01_Scripts/ExceptionManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Firebase;
+using UnityEngine;
+
+public class ExceptionManager
+{
+	public static void HandleFirebaseException(FirebaseException e)
+	{
+		switch (e.ErrorCode)
+		{
+			case 37:
+			case 38:
+				ShowPopup("이메일과 비밀번호를 입력해 주세요");
+				break;
+			case 1:
+				ShowPopup("이메일과 비밀번호가 일치하지 않거나\n존재하지 않는 이메일입니다.");
+				break;
+			case 11:
+				ShowPopup("이메일의 형식이 아닙니다.");
+				break;
+			default:
+				ShowPopup(e.Message);
+				break;
+		}
+	}
+
+	private static void ShowPopup(string message)
+	{
+		PanelManager.Instance.popUp.PopUpOpen(message,
+			() => PanelManager.Instance.popUp.PopUpClose());
+	}
+}
