9536cc020983a46c5710efcee061c4fbd753e8a7
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-15 21:04:40 +0900

    [추가] PartyPanel, PartyCreatePanel

---
+++ Assets/01_Scripts/Party/PartyPanel.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PartyPanel : MonoBehaviour
+{
+	public Text serverText;
+	public Button refreshButton;
+	public Button createButton;
+	public Button requestButton;
+	public Button cancelButton;
+
+	public PartyEntry partyEntryPrefab;
+	private string currentPartyId;
+
+	private List<PartyData> parties;
+	public RectTransform partyContent;
+
+	private Dictionary<string, Toggle> partyToggles = new Dictionary<string, Toggle>();
+
+	private void Awake()
+	{
+		refreshButton.onClick.AddListener(RefreshPartyList);
+		createButton.onClick.AddListener(CreateButtonClick);
+		requestButton.onClick.AddListener(RequestButtonClick);
+		cancelButton.onClick.AddListener(CancelButtonClick);
+	}
+
+	private void OnEnable()
+	{
+		serverText.text = $"{ServerManager.GetServerName()} 서버 파티 현황";
+		RefreshPartyList();
+	}
+
+	private void RefreshPartyList()
+	{
+		foreach (var VARIABLE in partyToggles)
+		{
+			Destroy(VARIABLE.Value.gameObject);
+		}
+
+		partyToggles.Clear();
+		// Update chat channels (partyId) and Display(Instantiate entry)
+		FirebaseManager.Instance.UpdateParty();
+		parties = FirebaseManager.Instance.GetPartyList();
+		if (parties != null)
+		{
+			foreach (var party in parties)
+			{
+				PartyEntry partyEntry = Instantiate(partyEntryPrefab, partyContent);
+				partyEntry.sizeText.text = party.party_size.ToString();
+				partyEntry.nameText.text = party.party_Name;
+				partyToggles[partyEntry.nameText.text] = partyEntry.GetComponent<Toggle>();
+			}
+		}
+	}
+
+	private void CreateButtonClick()
+	{
+		// 파티가 이미 있을 경우
+		if (FirebaseManager.Instance.CurrentUserData.user_CurrentParty != "")
+		{
+			UIManager.Instance.popUp.PopUpOpen("이미 파티에 가입되어 있습니다.",
+				() => UIManager.Instance.popUp.PopUpClose());
+		}
+		else
+		{
+			UIManager.Instance.OpenPartyCreatePanel();
+		}
+	}
+
+	private void RequestButtonClick()
+	{
+		foreach (var partyEntry in partyToggles)
+		{
+			if (partyEntry.Value.isOn)
+			{
+				FirebaseManager.Instance.JoinParty(partyEntry.Key);
+			}
+		}
+	}
+
+	private void CancelButtonClick()
+	{
+		gameObject.SetActive(false);
+	}
+}
