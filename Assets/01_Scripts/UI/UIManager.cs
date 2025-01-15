9d0a025cd9bf3f90e5e281cfc639ce482fb9d20f
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-15 11:09:06 +0900

    [변경] UIManager
    chat 추가

--- Assets/01_Scripts/UI/UIManager.cs
+++ Assets/01_Scripts/UI/UIManager.cs
@@ -1,120 +1,132 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : SingletonManager<UIManager>
 {
-    public ItemInfoPanel itemInfo;
-    public InventoryPanel inventory;
-    public ShopPanel shop;
-    public EquipmentPanel equipment;
-    public Inventory inventorySystem;
-    public PopUp popUp;
-    public InventorySlot slot;
-    [HideInInspector]public bool isInventoryActive = false;
-    [HideInInspector]public bool isItemInfoActive = false;
-    [HideInInspector]public bool isShopActive = false;
+	public ItemInfoPanel itemInfo;
+	public InventoryPanel inventory;
+	public ShopPanel shop;
+	public EquipmentPanel equipment;
+	public Inventory inventorySystem;
+	public PopUp popUp;
+	public InventorySlot slot;
+	[HideInInspector] public bool isInventoryActive = false;
+	[HideInInspector] public bool isItemInfoActive = false;
+	[HideInInspector] public bool isShopActive = false;
 
-    protected override void Awake()
-    {
-        base.Awake();
-        itemInfo = FindObjectOfType<ItemInfoPanel>();
-        inventory = FindObjectOfType<InventoryPanel>();
-        shop = FindObjectOfType<ShopPanel>();
-        equipment = FindObjectOfType<EquipmentPanel>();
-        inventorySystem = FindObjectOfType<Inventory>();
-        popUp = FindObjectOfType<PopUp>();
-    }
+	// Chat
+	public ChatPanel chatPanel;
+	public Button chatButton;
 
-    #region Inventory
-    
-    public void OpenInventoryPanel()
-    {
-        if (!IsAnyPanelOpen())
-        {
-            isInventoryActive = true;
-            inventory.panel.SetActive(true);
-        }
-    }
+	protected override void Awake()
+	{
+		base.Awake();
+		itemInfo = FindObjectOfType<ItemInfoPanel>();
+		inventory = FindObjectOfType<InventoryPanel>();
+		shop = FindObjectOfType<ShopPanel>();
+		equipment = FindObjectOfType<EquipmentPanel>();
+		inventorySystem = FindObjectOfType<Inventory>();
+		popUp = FindObjectOfType<PopUp>();
+		popUp.PopUpClose();
+	}
 
-    public void CloseInventoryPanel()
-    {
-        isInventoryActive = false;
-        inventory.panel.SetActive(false);
-    }
+	#region Inventory
 
-    #endregion
-    
-    #region Shop
+	public void OpenInventoryPanel()
+	{
+		if (!IsAnyPanelOpen())
+		{
+			isInventoryActive = true;
+			inventory.panel.SetActive(true);
+		}
+	}
 
-    public void OpenShopPanel()
-    {
-        if (!IsAnyPanelOpen())
-        {
-            isShopActive = true;
-            shop.shopPanel.SetActive(true);
-        }
-    }
+	public void CloseInventoryPanel()
+	{
+		isInventoryActive = false;
+		inventory.panel.SetActive(false);
+	}
 
-    public void CloseShopPanel()
-    {
-        isShopActive = false;
-        shop.RestoreOriginalState();
-        shop.shopPanel.SetActive(false);
-    }
-    
-    #endregion
+	#endregion
 
-    #region ItemInfo
+	#region Shop
 
-    public void OpenItemInfoPanel()
-    {
-        isItemInfoActive = true;
-        itemInfo.itemInfoPanel.SetActive(true);
-    }
+	public void OpenShopPanel()
+	{
+		if (!IsAnyPanelOpen())
+		{
+			isShopActive = true;
+			shop.shopPanel.SetActive(true);
+		}
+	}
 
-    public void CloseItemInfoPanel()
-    {
-        isItemInfoActive = false;
-        itemInfo.itemInfoPanel.SetActive(false);
-    }
-    
-    public void SetItemInfoScreen(ItemBase item)
-    {
-        if (item.itemClass == 1)
-        {
-            itemInfo.firstOptionButton.enabled = true;
-            itemInfo.secondOptionButton.enabled = true;
-            itemInfo.secondOption.SetActive(true);
-            itemInfo.firstOptionText.text = "장착";
-        }
+	public void CloseShopPanel()
+	{
+		isShopActive = false;
+		shop.RestoreOriginalState();
+		shop.shopPanel.SetActive(false);
+	}
 
-        if (item.itemClass == 2)
-        {
-            itemInfo.firstOptionButton.enabled = false;
-            itemInfo.secondOptionButton.enabled = true;
-            itemInfo.secondOption.SetActive(true);
-            itemInfo.firstOptionText.text = item.currentItemCount + "개";
-        }
+	#endregion
 
-        if (item.itemClass == 3)
-        {
-            itemInfo.firstOptionButton.enabled = false;
-            itemInfo.secondOption.SetActive(false);
-            itemInfo.firstOptionText.text = item.currentItemCount + "개";
-        }  
-        
-        itemInfo.itemName.text = item.name.ToString();
-        itemInfo.itemDescription.text = item.description.ToString();
-        itemInfo.itemImage.sprite = item.image;
-        
-    }
-    
-    #endregion
-    
-    public bool IsAnyPanelOpen()
-    {
-        return isInventoryActive || isItemInfoActive || isShopActive;
-    }
+	#region ItemInfo
+
+	public void OpenItemInfoPanel()
+	{
+		isItemInfoActive = true;
+		itemInfo.itemInfoPanel.SetActive(true);
+	}
+
+	public void CloseItemInfoPanel()
+	{
+		isItemInfoActive = false;
+		itemInfo.itemInfoPanel.SetActive(false);
+	}
+
+	public void SetItemInfoScreen(ItemBase item)
+	{
+		if (item.itemClass == 1)
+		{
+			itemInfo.firstOptionButton.enabled = true;
+			itemInfo.secondOptionButton.enabled = true;
+			itemInfo.secondOption.SetActive(true);
+			itemInfo.firstOptionText.text = "장착";
+		}
+
+		if (item.itemClass == 2)
+		{
+			itemInfo.firstOptionButton.enabled = false;
+			itemInfo.secondOptionButton.enabled = true;
+			itemInfo.secondOption.SetActive(true);
+			itemInfo.firstOptionText.text = item.currentItemCount + "개";
+		}
+
+		if (item.itemClass == 3)
+		{
+			itemInfo.firstOptionButton.enabled = false;
+			itemInfo.secondOption.SetActive(false);
+			itemInfo.firstOptionText.text = item.currentItemCount + "개";
+		}
+
+		itemInfo.itemName.text = item.name.ToString();
+		itemInfo.itemDescription.text = item.description.ToString();
+		itemInfo.itemImage.sprite = item.image;
+	}
+
+	#endregion
+
+	public bool IsAnyPanelOpen()
+	{
+		return isInventoryActive || isItemInfoActive || isShopActive;
+	}
+
+	#region Chat
+
+	public void ChatButtonClick()
+	{
+		chatPanel.gameObject.SetActive(true);
+		chatButton.gameObject.SetActive(false);
+	}
+
+	#endregion
 }
