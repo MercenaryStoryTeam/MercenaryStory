15b96153895c2d24fa89809b359c39d044547392
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-15 21:03:58 +0900

    [추가] PartyData, PartyEntry
    파티 데이터 관련 스크립트

---
+++ Assets/01_Scripts/Party/PartyData.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class PartyData
+{
+	public string party_Server { get; set; }
+	public string party_Name { get; set; }
+	public int party_size { get; set; }
+	public UserData party_Owner { get; set; }
+	public List<UserData> party_Members { get; set; }
+
+	public PartyData()
+	{
+	}
+
+	// CreateParty
+	public PartyData(string server, string name, int size, UserData owner)
+	{
+		party_Server = server;
+		party_Name = name;
+		party_size = size;
+		party_Owner = owner;
+		party_Members = new List<UserData>();
+		party_Members.Add(owner);
+	}
+
+	// AddMember
+	public void AddMember(UserData userdata)
+	{
+		party_Members.Add(userdata);
+	}
+}
---
+++ Assets/01_Scripts/Party/PartyEntry.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PartyEntry : MonoBehaviour
+{
+	public Text sizeText;
+	public Text nameText;
+}
