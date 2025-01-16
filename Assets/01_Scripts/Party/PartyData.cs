d108273ebd6cb05f474973c501862af829b8bce1
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-16 16:13:29 +0900

    [변경] PartyData
    party_Id field 추가

--- Assets/01_Scripts/Party/PartyData.cs
+++ Assets/01_Scripts/Party/PartyData.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 [Serializable]
 public class PartyData
 {
+	public string party_Id { get; set; }
 	public string party_Server { get; set; }
 	public string party_Name { get; set; }
 	public int party_size { get; set; }
@@ -17,6 +18,7 @@ public class PartyData
 	// CreateParty
 	public PartyData(string server, string name, int size, UserData owner)
 	{
+		party_Id = Guid.NewGuid().ToString();
 		party_Server = server;
 		party_Name = name;
 		party_size = size;
