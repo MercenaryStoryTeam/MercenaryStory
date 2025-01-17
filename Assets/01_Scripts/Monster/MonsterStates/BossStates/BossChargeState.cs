02c0f59e8e280ae733b94d5b25284efd470f7ab8
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-01-17 14:53:05 +0900

    [변경] BossChargeState 복구

--- Assets/01_Scripts/Monster/MonsterStates/BossStates/BossChargeState.cs
+++ Assets/01_Scripts/Monster/MonsterStates/BossStates/BossChargeState.cs
@@ -1,17 +1,26 @@
+using UnityEngine;
+
 public class BossChargeState : BossState
 {
     public override void EnterState(BossMonster boss)
     {
-        throw new System.NotImplementedException();
+        int targetNum = Random.Range(0,boss.playerList.Count);
+        boss.TargetTransform = boss.playerList[targetNum].transform;
+        boss.StartCoolDown();
+        boss.Animator.SetBool("Charge",true);
     }
 
     public override void ExecuteState(BossMonster boss)
     {
-        throw new System.NotImplementedException();
+        boss.Agent.SetDestination(boss.TargetTransform.position);
+        if (boss.Agent.remainingDistance <= boss.Agent.stoppingDistance)
+        {
+            boss.ChangeState(BossStateType.Idle);
+        }
     }
 
     public override void ExitState(BossMonster boss)
     {
-        throw new System.NotImplementedException();
+        boss.Animator.SetBool("Charge",false);
     }
 }
