045f830c72fbb959a84ab249f641ab56e5c54083
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-01-17 17:29:57 +0900

    [변경] DetectCollider 수정

--- Assets/01_Scripts/Map/DetectCollider.cs
+++ Assets/01_Scripts/Map/DetectCollider.cs
@@ -7,28 +7,35 @@ public class DetectCollider : MonoBehaviour
 {
     public BossMonster boss;
     public List<Minion> minions;
+    public List<Player> players;
 
     private void Update()
     {
-        minions = boss.minionList;
+        foreach (Minion minion in minions)
+        {
+            minion.playerList = players;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Minion"))
+             {
+                 print(other.gameObject.name);
+                 minions.Add(other.GetComponent<Minion>());
+                 boss.minionList.Add(other.GetComponent<Minion>());
+             }
         if (other.CompareTag("Player"))
         {
             print(other.gameObject.name);
+            players.Add(other.gameObject.GetComponent<Player>());
             boss.playerList.Add(other.GetComponent<Player>());
             foreach (Minion minion in minions)
             {
                 minion.playerList.Add(other.GetComponent<Player>());
             }
         }
-        if (other.CompareTag("Minion"))
-        {
-            print(other.gameObject.name);
-            boss.minionList.Add(other.GetComponent<Minion>());
-        }
+        
     }
 
     private void OnTriggerExit(Collider other)
