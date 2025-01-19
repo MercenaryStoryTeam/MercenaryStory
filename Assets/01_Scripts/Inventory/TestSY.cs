35852476d923d8f91b7fb93b2a2c0d8451b6d4f9
장소영 <soyoung7896@naver.com>
2025-01-20 00:04:24 +0900

    [변경] Player Test SY Scene
    나중에 Player 스크립트에 추가 예정
    아이템 드롭 관련 변경

--- Assets/01_Scripts/Inventory/TestSY.cs
+++ Assets/01_Scripts/Inventory/TestSY.cs
@@ -10,22 +10,70 @@ public class TestSY : MonoBehaviour
     public float myGold = 0;
     
     private PlayerTestSY playerTest;
-    private void Start ()
+    private bool isPlayerSpawned = false;
+    
+    
+    private void Start()
     {
-        playerTest = FindObjectOfType<PlayerTestSY>();
     }
 
     private void Update()
     {
+        if(!isPlayerSpawned)
+        {
+            playerTest = FindObjectOfType<PlayerTestSY>();
 
-        UIManager.Instance.inventory.TryOpenInventory();
-        UIManager.Instance.shop.TryOpenShop();
-        
+            if(playerTest != null )
+            {
+                isPlayerSpawned = true;
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.L))
         {
-            ItemBase randomItem = InventoryManger.Instance.RandomDropItems();
-            playerTest.InvenSceneTestDrop(randomItem);
+            ItemBase randomItem = TryBossDropItem(InventoryManger.Instance.allItems);
+            if (randomItem != null)
+            {
+                if (playerTest != null)
+                {
+                    playerTest.InvenSceneTestDrop(randomItem);
+                }
+                else
+                {
+                    Debug.Log("PlayerTestSY 스크립트 참조 X");
+                }
+            }
         }
     }
-    
+
+    private ItemBase TryBossDropItem(List<ItemBase> items)
+    {
+        ItemBase droppedItem = null;
+
+        float dropChance = UnityEngine.Random.Range(0f, 1f);
+        if (dropChance <= 0.5f)
+        {
+            foreach (ItemBase item in items)
+            {
+                float randomValue = UnityEngine.Random.Range(0f, 1f);
+                if (randomValue <= item.dropPercent)
+                {
+                    Debug.Log($"{dropChance}의 확률로 아이템 획득!");
+
+                    droppedItem = item;
+                    Debug.Log($"아이템 {droppedItem}을 {randomValue}의 확률로 얻음!");
+
+                    break;
+                }
+            }
+        }
+
+        else
+        {
+            Debug.Log($"{dropChance}의 확률로 아이템을 획득하지 못함");
+        }
+
+        return droppedItem;
+    }
+
 }
