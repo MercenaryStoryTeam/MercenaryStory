4f0f94aada6169e430b40fc40d225d155e6c4ed6
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-22 14:36:32 +0900

    [변경] Portal
    LoadScene -> LeaveAndLoadScene

--- Assets/01_Scripts/Map/Portal.cs
+++ Assets/01_Scripts/Map/Portal.cs
@@ -7,57 +7,59 @@ using UnityEngine;
 [RequireComponent(typeof(MeshRenderer))]
 public class Portal : MonoBehaviour
 {
-    public MeshRenderer portalRenderer;
-    public float additionalHeight =0.5f;
-    public int layerMask;
+	public MeshRenderer portalRenderer;
+	public float additionalHeight = 0.5f;
+	public int layerMask;
 
-    private void Start()
-    {
-        layerMask = LayerMask.GetMask("Player");
-        portalRenderer = GetComponent<MeshRenderer>();
-    }
+	private void Start()
+	{
+		layerMask = LayerMask.GetMask("Player");
+		portalRenderer = GetComponent<MeshRenderer>();
+	}
 
-    private void Update()
-    {
-        IsOnPortal();
-    }
+	private void Update()
+	{
+		IsOnPortal();
+	}
 
-    private void IsOnPortal()
-    {
-        if (!PhotonNetwork.IsMasterClient) return;
-        
-        Bounds bounds = portalRenderer.bounds;
-        Vector3 center = bounds.center;
-        Vector3 size = bounds.extents;
+	private void IsOnPortal()
+	{
+		if (!PhotonNetwork.IsMasterClient) return;
 
-        size.y += additionalHeight / 2f;
+		Bounds bounds = portalRenderer.bounds;
+		Vector3 center = bounds.center;
+		Vector3 size = bounds.extents;
 
-        Collider[] hitColliders = Physics.OverlapBox(center, size, gameObject.transform.rotation, layerMask);
-        {
-            foreach (var hitCollider in hitColliders)
-            { 
-                if (hitCollider.gameObject == StageManager.Instance.hostPlayerFsm.gameObject && StageManager.Instance.StageClear)
-                {
-                    ServerManager.LoadScene("");
-                } 
-            }
-        }
-    }
+		size.y += additionalHeight / 2f;
 
-    private void OnDrawGizmos()
-    {
-        if (portalRenderer == null)
-            portalRenderer = gameObject.GetComponent<MeshRenderer>();
+		Collider[] hitColliders =
+			Physics.OverlapBox(center, size, gameObject.transform.rotation, layerMask);
+		{
+			foreach (var hitCollider in hitColliders)
+			{
+				if (hitCollider.gameObject == StageManager.Instance.hostPlayerFsm.gameObject &&
+				    StageManager.Instance.StageClear)
+				{
+					ServerManager.LeaveAndLoadScene("");
+				}
+			}
+		}
+	}
 
-        Bounds bounds = portalRenderer.bounds;
-        Vector3 center = bounds.center;
-        Vector3 size = bounds.extents;
+	private void OnDrawGizmos()
+	{
+		if (portalRenderer == null)
+			portalRenderer = gameObject.GetComponent<MeshRenderer>();
 
-        // 박스의 높이 조정
-        size.y += additionalHeight / 2f;
+		Bounds bounds = portalRenderer.bounds;
+		Vector3 center = bounds.center;
+		Vector3 size = bounds.extents;
 
-        Gizmos.color = Color.red;
-        Gizmos.matrix = Matrix4x4.TRS(center, gameObject.transform.rotation, Vector3.one);
-        Gizmos.DrawWireCube(Vector3.zero, size * 2);
-    }
+		// 박스의 높이 조정
+		size.y += additionalHeight / 2f;
+
+		Gizmos.color = Color.red;
+		Gizmos.matrix = Matrix4x4.TRS(center, gameObject.transform.rotation, Vector3.one);
+		Gizmos.DrawWireCube(Vector3.zero, size * 2);
+	}
 }
