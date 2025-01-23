b5979258b94ac03f4cf18fb0af072c2532c800cf
ztazan2 <ztazan2@gmail.com>
2025-01-23 16:14:56 +0900

    [추가] 조이스틱
    조이스틱을 이용한 플레이어 이동 가능

--- Assets/01_Scripts/PlayerCamera+Move/New Folder/VirtualJoystick.cs
+++ Assets/01_Scripts/PlayerCamera+Move/New Folder/VirtualJoystick.cs
@@ -9,15 +9,19 @@ public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, I
     [Header("조이스틱 핸들 RectTransform")]
     public RectTransform joystickHandle;
 
-    [Header("조이스틱 핸들의 이동 범위 (최대값)")]
-    public float joystickRange = 45f; 
+    [Header("조이스틱 핸들의 이동 범위")]
+    public float joystickRange = 35f;
+
+    [Header("Dead Zone")]
+    public float deadZone = 0.2f;
 
     // 입력 벡터 (조이스틱의 방향과 크기)
-    private Vector2 inputVector; 
+    private Vector2 inputVector;
 
     // 입력 벡터를 외부에서 접근할 수 있도록 공개
-    public Vector2 InputVector => inputVector; 
+    public Vector2 InputVector => inputVector;
 
+    // 조이스틱 핸들러 이동
     public void OnDrag(PointerEventData eventData)
     {
         Vector2 pos;
@@ -33,6 +37,12 @@ public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, I
             inputVector = new Vector2(pos.x * 2, pos.y * 2);
             inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
 
+            // Dead Zone 적용, 작은 입력값 무시 -> 핸들러 원점 근처에서 불필요한 입력을 발생시키는 문제를 방지
+            if (inputVector.magnitude < deadZone)
+            {
+                inputVector = Vector2.zero;
+            }
+
             // 조이스틱 핸들의 위치를 joystickRange 범위 안으로 제한
             joystickHandle.anchoredPosition = new Vector2(
                 Mathf.Clamp(inputVector.x * joystickRange, -joystickRange, joystickRange),
@@ -40,10 +50,11 @@ public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, I
         }
     }
 
+    // 조이스틱 핸들러 이동 
     public void OnPointerDown(PointerEventData eventData)
     {
         // 드래그 이벤트 호출
-        OnDrag(eventData); 
+        OnDrag(eventData);
     }
 
     public void OnPointerUp(PointerEventData eventData)
