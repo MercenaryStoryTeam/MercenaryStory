b3fbe6dea9180c35b109d3d6b250b380dd33478e
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-24 06:57:45 +0900

    [변경] Portal
    파티 미가입 시 팝업 띄우기

--- Assets/01_Scripts/Map/Portal.cs
+++ Assets/01_Scripts/Map/Portal.cs
@@ -6,7 +6,12 @@ public class Portal : MonoBehaviour
 	private void OnTriggerEnter(Collider other)
 	{
 		if (StageManager.Instance.portalIsActive) return;
-		if (FirebaseManager.Instance.CurrentPartyData == null) return;
+		if (FirebaseManager.Instance.CurrentPartyData == null)
+		{
+			UIManager.Instance.popUp.PopUpOpen("파티에 가입하고 참여해 주세요.",
+				() => UIManager.Instance.popUp.PopUpClose());
+			return;
+		}
 
 		if (StageManager.Instance.currentStage == 0 &&
 		    FirebaseManager.Instance.CurrentPartyData.party_Owner.user_Name ==
