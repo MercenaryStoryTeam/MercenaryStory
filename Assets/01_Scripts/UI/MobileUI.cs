c67c4908d5bdabf1b16f55097e8e1262c1ba9fcc
Soyoung <soyoung7896@naver.com>
2025-01-24 16:05:48 +0900

    [추가] Moblie UI Script
    및 관련 프리팹 추가,

---
+++ Assets/01_Scripts/UI/MobileUI.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MobileUI : MonoBehaviour
+{
+    public Button invenButton;
+    public Button optionButton;
+    
+    public Button interactButton; //플레이어 인풋 매니저 E키 그대로 사용
+
+    private void Awake()
+    {
+        MoblieUIOnClick();
+    }
+
+    private void MoblieUIOnClick()
+    {
+        invenButton.onClick.AddListener(InvenButtonClicked);
+        optionButton.onClick.AddListener(OptionButtonClicked);
+    }
+    
+    private void InvenButtonClicked()
+    {
+        UIManager.Instance.OpenInventoryPanel();
+    }
+
+    private void OptionButtonClicked()
+    {
+        UIManager.Instance.OpenOptionPanel();
+    }
+}
