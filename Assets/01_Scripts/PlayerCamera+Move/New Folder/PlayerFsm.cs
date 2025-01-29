32d2f887fc7f05a4d95689b16a46776a069fccca
wkdtjsdndlqslek <ehdtk456@naver.com>
2025-01-29 18:08:59 +0900

    [변경] PlayerFsm 변경

--- Assets/01_Scripts/PlayerCamera+Move/New Folder/PlayerFsm.cs
+++ Assets/01_Scripts/PlayerCamera+Move/New Folder/PlayerFsm.cs
@@ -39,7 +39,7 @@ public class PlayerFsm : MonoBehaviourPun
 		Hit
 	}
 
-	private State currentState = State.Idle;
+	public State currentState = State.Idle;
 	private bool isDead = false;
 
 	// 콤보 시스템
