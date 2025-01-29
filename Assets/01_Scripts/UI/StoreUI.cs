440ed8bc5ab542b8260c7ae92f6c947d92be786e
JiwonEE2 <jiwonchichi1@gmail.com>
2025-01-29 17:11:16 +0900

    [변경] LJW_TownScene
    Store UI 추가 및 상점 오브젝트에 상점 텍스트 추가

---
+++ Assets/01_Scripts/UI/StoreUI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreUI : MonoBehaviour
+{
+	public TextMesh textMesh;
+	public GameObject Cam;
+	// public Player player;
+
+	Vector3 startScale;
+	public float distance = 350;
+
+	void Start()
+	{
+		// player = gameObject.GetComponentInParent<Player>();
+		textMesh = gameObject.GetComponent<TextMesh>();
+		Cam = GameObject.FindGameObjectWithTag("VirtualCamera");
+		startScale = transform.localScale;
+	}
+
+	void Update()
+	{
+		float dist = Vector3.Distance(Cam.transform.position, transform.position);
+		Vector3 newScale = startScale * dist / distance;
+		transform.localScale = newScale;
+
+		transform.rotation = Cam.transform.rotation;
+	}
+}
